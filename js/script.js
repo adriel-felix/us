9d4b80123c6ba72eb1c88bd0c8b62b9e3041baa7
Adriel Felix <adrielandrade3@gmail.com>
2023-12-24 06:13:55 -0300

    finished

--- js/script.js
+++ js/script.js
@@ -58,6 +58,17 @@ audio.onended = function() {
   audio.play();
 }
 
+window.onscroll = function() {
+  audio.play();
+}
+
+window.onwheel = function(event) {
+  if (event.button === 1){
+    audio.play();
+  }
+}
+
+
 function changeMusic(musicName) {
   audio.setAttribute("src", `./assets/audio/${musicName}.mp3`);
   audio.load();
@@ -102,4 +113,111 @@ function onMouseOverTest(btn) {
       isAnimating = false;
     }, 200);
   }
+}
+
+let hiddenText = document.getElementById('hiddenText');
+let hiddenText2 = document.getElementById('hiddenText2');
+let hiddenText3 = document.getElementById('hiddenText3');
+let hiddenText4 = document.getElementById('hiddenText4');
+function showLoveText() {
+
+  let fireworkPlayer = document.getElementById("fireworkPlayer");
+  fireworkPlayer.volume = 0.2;
+  fireworkPlayer.play();
+
+  hiddenText.style.display = 'block';
+  hiddenText.innerHTML = '';
+
+  let fullText = 'Meu amor, eu te amo tanto!';
+  let index = 0;
+
+  function showChar() {
+    if (index < fullText.length) {
+      hiddenText.innerHTML += fullText.charAt(index);
+      index++;
+      setTimeout(showChar, 100);
+    } else {
+      setTimeout(showLoveText2, 1000)
+    }
+  }
+
+  showChar();
+}
+
+function showLoveText2() {
+  
+  hiddenText.style.display = 'none';
+  hiddenText2.style.display = 'block';
+  hiddenText2.innerHTML = '';
+
+  let fullText = 'Obrigado por ser essa mulher incrível.';
+  let index = 0;
+
+  function showChar2() {
+    if (index < fullText.length) {
+      hiddenText2.innerHTML += fullText.charAt(index);
+      index++;
+      setTimeout(showChar2, 100);
+    } else {
+      setTimeout(showLoveText3, 1000)
+    }
+  }
+
+  showChar2();
+}
+
+function showLoveText3() {
+
+  hiddenText2.style.display = 'none';
+  hiddenText3.style.display = 'block';
+  hiddenText3.innerHTML = '';
+
+  let fullText = 'Quero passar o resto da minha vida ao seu lado, criando lindas memórias e colecionando momentos únicos.';
+  let index = 0;
+
+  function showChar3() {
+    if (index < fullText.length) {
+      hiddenText3.innerHTML += fullText.charAt(index);
+      index++;
+      setTimeout(showChar3, 100);
+    } else {
+      setTimeout(showLoveText4, 1000)
+    }
+  }
+
+  showChar3();
+}
+
+function showLoveText4() {
+
+  hiddenText3.style.display = 'none';
+  hiddenText4.style.display = 'block';
+  hiddenText4.innerHTML = '';
+
+  let fullText = 'TE AMO PARA SEMPRE! ❤';
+  let index = 0;
+
+  function showChar4() {
+    if (index < fullText.length) {
+      hiddenText4.innerHTML += fullText.charAt(index);
+      index++;
+      setTimeout(showChar4, 100);
+    } else {
+      setTimeout(function () {
+        let celebrationModal = document.getElementById('celebrationModal');
+        let modal = bootstrap.Modal.getInstance(celebrationModal)
+        modal.hide();
+        hideLoveText();
+      }, 5000)
+    }
+  }
+
+  showChar4();
+}
+
+function hideLoveText() {
+  hiddenText.style.display = 'none';
+  hiddenText2.style.display = 'none';
+  hiddenText3.style.display = 'none';
+  hiddenText4.style.display = 'none';
 }
